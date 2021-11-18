c8570702ee42432aab36ad014288dfb1c534e67f
VladisssLuv <vladissslav19@gmail.com>
2021-11-18 21:34:45 +0300

    ready-made calculation class 1.0

--- calculationsf.py
+++ calculationsf.py
@@ -2,7 +2,6 @@ class Calculate(object):
     def __init__(self):
         pass
     def сalculate_without_reinf(self, h, a, b, N, M_sup, M_inf, x0, R_bt, h0=None):
-        result = [False, False] 
         if h0 is None:
             h0 = h - 30
         
@@ -18,22 +17,23 @@ class Calculate(object):
         Fe0 = N * e0/1000 
         M -= Fe0
         if N*1000/u + M*10**6/W_b <= R_bt*h0:
-           result[0] = True
+            u = 2*(a+b+2*h0)
+            W_b = (a+h0)*((a+h0)/3+b+h0)
+            M = M_loc/2
+            if N*1000/u + M*10**6/W_b <= R_bt*h0:
+                return 'The cross-sectional strength is ensured. Without rods.'
+            else:
+                return 'The cross-section sleep rate is not guaranteed.'
+        else:
+            return 'The cross-section sleep rate is not guaranteed.'
 
-        u = 2*(a+b+2*h0)
-        W_b = (a+h0)*((a+h0)/3+b+h0)
-        M = M_loc/2
-        if N*1000/u + M*10**6/W_b <= R_bt*h0:
-            result[1] = True
 
-        return result
-
-    def сalculate_with_reinf(self, h, a, b, M_xsup, M_ysup, M_xinf, M_yinf, s_w, R_bt, h0=None):
-        result = []
+    def сalculate_with_reinf(self, h, a, b, N, M_xsup, M_ysup, M_xinf, M_yinf, s_w, R_bt, h0=None):
         if h0 is None:
             h0 = h - 30
         
-        u, W_bx, W_by = self.__fill_UWW(a, b)
+        F = N
+        u, W_bx, W_by = self.__fill_UWW(a, b, h0)
         M_x = (M_xsup + M_xinf) / 2 
         M_y = (M_ysup + M_yinf) / 2
         s_w_count = h0 // s_w  
@@ -43,10 +43,46 @@ class Calculate(object):
         min_diameter = 6
         A_sw = round(R_sw / (min_diameter / (s_w_count - 1)))
         q_sw = round((R_sw * A_sw) / s_w, 1) 
-        # продолжить расчёты...
+        
+        return_line = ''
+        if (M_x / W_bx) + (M_y / W_by) > F / u:
+            return_line += 'An adjustment has been made '
+            difference = ((M_x / W_bx) + (M_y / W_by)) - (F / u)
+            if (M_x / W_bx) > (M_y / W_by):
+                need_to_get = M_x - (W_bx * difference)
+                if M_xinf > M_xsup:
+                    M_xinf = need_to_get * 2 - M_xsup
+                    return_line += 'M_xinf. New value: %s' % M_xinf
+                else:
+                    M_xsup = need_to_get * 2 - M_xinf
+                    return_line += 'M_xsup. New value: %s' % M_xsup
+                M_x = (M_xsup + M_xinf) / 2
+            else:
+                need_to_get = M_y - (W_by * difference)
+                if M_yinf > M_ysup:
+                    M_yinf = need_to_get * 2 - M_ysup
+                    return_line += 'M_yinf. New value: %s' % M_yinf
+                else:
+                    M_ysup = need_to_get * 2 - M_yinf
+                    return_line += 'M_ysup. New value: %s' % M_ysup
+                M_y = (M_ysup + M_yinf) / 2
+            return_line += '\n'
+        if (F / u) + (M_x / W_bx) + (M_y / W_by) > R_bt * h0:
+            return 'Strength is not ensured, the separation area should be increased.'
+        else:
+            if 0.25 * R_bt * h0 > q_sw:
+                q_sw = 0 
+            if (F / u) + (M_x / W_bx) + (M_y / W_by) < R_bt * h0 + 0.8 * q_sw:  
+                a_new = a + 2 * (second_row + 4 * s_w) + h0
+                b_new = b + 2 * (second_row + 4 * s_w) + h0
+                u_new, W_bx_new, W_by_new = self.__fill_UWW(a_new, b_new, h0)
+                if ((F * 10 ** 3) / u_new) + ((M_x * 10 ** 6) / W_bx_new) + \
+                        ((M_y * 10 ** 6) / W_by_new) < R_bt * h0:  
+                    return return_line + 'The cross-sectional strength is ensured. Members:% s; Distance between bars:% s;' % \
+                        (s_w_count - 1, first_row)
+        return 'Conditions not met!'
 
-    
-    def __fill_UWW(a_def, b_def, h0):
+    def __fill_UWW(self, a_def, b_def, h0):
         u_def = round(2 * (a_def + b_def + 2 * h0))
         W_bx_def = round((a_def + h0) * ((a_def + h0) / 3 + b_def + h0))
         W_by_def = round((b_def + h0) * ((b_def + h0) / 3 + a_def + h0))
