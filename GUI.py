71773ff40bc0d20dca328be016c826087ad45bd9
LorexIQ <murashko100500@gmail.com>
2021-11-20 17:51:13 +0300

    Создан файл, для отладки класса интерфейса. Так же написана часть интерфеса, в которою включены: класс для быстрого создания lineEdit с custom дизайном; цикл, для быстрого создания lineEdit по заданным параметрам в одну строку; проверка введённых данных в lineEdit и реакция на них; общая проверка введённых данных, для продолжения расчётов.

--- GUI.py
+++ GUI.py
@@ -5,7 +5,7 @@ class CustonLineEdit:
     def __init__(self):
         self.state = False
 
-    def initUi(self, set_coord, title, title_type, centralwidget):
+    def initUi(self, set_coord, title, title_type, font_size, centralwidget):
         x, y = set_coord
         font = QtGui.QFont()
         font.setFamily("Open Sans Condensed")
@@ -20,7 +20,7 @@ class CustonLineEdit:
         self.lineEdit = QtWidgets.QLineEdit(centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(x + 75, y + 2, 126, 41))
         self.lineEdit.setFont(font)
-        self.lineEdit.setStyleSheet("border: 0;\n"
+        self.lineEdit.setStyleSheet("border: 0;"
                                     "background-color: rgba(0,0,0,0);")
         self.lineEdit.setMaxLength(10)
         self.lineEdit.setObjectName("lineEdit")
@@ -36,6 +36,7 @@ class CustonLineEdit:
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(label_object.sizePolicy().hasHeightForWidth())
+        font.setPointSize(font_size)
         label_object.setSizePolicy(sizePolicy)
         label_object.setFont(font)
         label_object.setAlignment(QtCore.Qt.AlignCenter)
@@ -67,40 +68,86 @@ class CustonLineEdit:
 
 
 class Ui_MainWindow(object):
+    def __init__(self):
+        self.status_mode = 1
+
     def setupUi(self, MainWindow):
+        def initWindowMode(info_edit_lines):
+            window = QtWidgets.QWidget(self.centralwidget)
+            window.setGeometry(0, 50, 545, 435)
+            array_lines = []
+            for i in info_edit_lines:
+                array_lines.append(CustonLineEdit())
+                array_lines[-1].initUi(*i, window)
+            return window, array_lines
+
+        def initButtonMode(title, widget, layout, css):
+            font = QtGui.QFont()
+            font.setFamily("Open Sans Condensed")
+            font.setPointSize(14)
+            font.setBold(False)
+            font.setWeight(50)
+            button = QtWidgets.QPushButton(widget)
+            button.setObjectName(title)
+            button.setMinimumSize(QtCore.QSize(150, 35))
+            button.setStyleSheet(css)
+            button.setText(title)
+            button.setFont(font)
+            button.clicked.connect(self.change_mode)
+            layout.addWidget(button)
+            return button
+
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 444)
+        MainWindow.resize(800, 550)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        info_edit_lines = [[(40, 70), 'h', 'm'],
-                           [(40, 120), 'k', 'mm'],
-                           [(40, 170), 'l', 'H']]
-        self.array_lines = []
-
-        for i in info_edit_lines:
-            self.array_lines.append(CustonLineEdit())
-            self.array_lines[-1].initUi(*i, self.centralwidget)
-
         self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(240, 10, 195, 30))
+        self.widget.setGeometry(QtCore.QRect(200, 15, 309, 41))
         self.widget.setObjectName("widget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setSpacing(2)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton = QtWidgets.QPushButton(self.widget)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QtWidgets.QPushButton(self.widget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.with_reinf_button = initButtonMode('WITH REINF', self.widget, self.horizontalLayout,
+                                                "background-color: #97ff88; border: 1px solid #000;")
+        self.without_reinf_button = initButtonMode('WITHOUT REINF', self.widget, self.horizontalLayout,
+                                                   "background-color: #fff; border: 1px solid #000;")
+
+        self.with_reinf, self.with_reinf_lines = initWindowMode([[(20, 20), 'a', 'мм', 16],
+                                                                 [(20, 70), 'b', 'мм', 16],
+                                                                 [(275, 70), 'b', 'мм', 16],
+                                                                 [(20, 120), 'N', 'кН', 16],
+                                                                 [(20, 170), 'M_x.sup', 'кН.м', 12],
+                                                                 [(275, 170), 'M_y.sup', 'кН.м', 12],
+                                                                 [(20, 220), 'M_x.inf', 'кН.м', 12],
+                                                                 [(275, 220), 'M_y.inf', 'кН.м', 12],
+                                                                 [(20, 270), 's_w', 'мм', 16],
+                                                                 [(20, 320), 'R_bt', 'МПа', 16],
+                                                                 [(20, 370), 'h_0', 'мм', 16]])
+
+        self.without_reinf, self.without_reinf_lines = initWindowMode([[(20, 20), 'a', 'мм', 16],
+                                                                       [(20, 70), 'b', 'мм', 16],
+                                                                       [(275, 70), 'b', 'мм', 16],
+                                                                       [(20, 120), 'N', 'кН', 16],
+                                                                       [(20, 170), 'M_sup', 'кН.м', 16],
+                                                                       [(275, 170), 'M_sup', 'кН.м', 16],
+                                                                       [(20, 220), 'x_0', 'мм', 16],
+                                                                       [(20, 270), 'R_bt', 'МПа', 16],
+                                                                       [(20, 320), 'h_0', 'мм', 16]])
+        self.with_reinf.hide()
+
         self.start_calculations = QtWidgets.QPushButton(self.centralwidget)
+        self.start_calculations.setGeometry(350, 450, 200, 40)
         self.start_calculations.setText('Тест')
         self.start_calculations.clicked.connect(self.submit_enter)
-        MainWindow.setCentralWidget(self.centralwidget)
 
+        MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def change_mode(self):
+        pass
 
     def submit_enter(self):
         for i in self.array_lines:
@@ -111,5 +158,3 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
