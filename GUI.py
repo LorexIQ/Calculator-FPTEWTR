d8c873941943d89b165227bb5f1bf58c5b12170f
LorexIQ <murashko100500@gmail.com>
2021-11-23 01:51:31 +0300

    Добавлены кнопки сохранения, загрузки. Переработана механика блокировки кнопок на меню результатов. Написаны головы функций чтения|записи (требует написания тела)

--- GUI.py
+++ GUI.py
@@ -4,12 +4,10 @@ import pickle
 
 
 class ResultMenu(QtWidgets.QWidget):
-    def __init__(self, button_1, button_2, info, checkbox, *args):
-        QtWidgets.QWidget.__init__(self, *args)
-        self._button_1 = button_1
-        self._button_2 = button_2
-        self._checkbox = checkbox
-        self._info = info
+    def __init__(self, *args):
+        QtWidgets.QWidget.__init__(self, *args[6:])
+        self._objects = args[:6]
+        self._timed_line = ''
         self._initUi()
 
     def _initUi(self):
@@ -52,17 +50,16 @@ class ResultMenu(QtWidgets.QWidget):
         self._title.setText(title)
 
     def call(self):
-        self._checkbox.setEnabled(False)
-        self._button_1.setEnabled(False)
-        self._button_2.setEnabled(False)
-        self._info.hide()
+        for object_red in self._objects:
+            object_red.setEnabled(False)
+        self._timed_line = self._objects[2].toolTip()
+        self._objects[2].setToolTip('')
         self.show()
 
     def close_win(self):
-        self._checkbox.setEnabled(True)
-        self._button_1.setEnabled(True)
-        self._button_2.setEnabled(True)
-        self._info.show()
+        for object_red in self._objects:
+            object_red.setEnabled(True)
+        self._objects[2].setToolTip(self._timed_line)
         self.hide()
 
 
@@ -100,14 +97,6 @@ class CustonLineEdit:
         self._label_object.setText(title)
         self._background.setToolTip('test')
 
-    def editUnit(self, new_unit, size=16):
-        self._font.setPointSize(size)
-        self._label_unit.setFont(self._font)
-        self._label_unit.setText(new_unit)
-
-    def getObject(self):
-        return self._label_object.text()
-
     def _editState(self, state):
         self._state = False
         if state == 1:
@@ -120,6 +109,11 @@ class CustonLineEdit:
         elif state == 4:
             self._state = True
             self._background.setPixmap(QtGui.QPixmap("imgs/lineEdit/falsed.jpg"))
+
+    def editUnit(self, new_unit, size=16):
+        self._font.setPointSize(size)
+        self._label_unit.setFont(self._font)
+        self._label_unit.setText(new_unit)
 
     def _format_line(self):
         t = self._lineEdit.text()
@@ -140,11 +134,17 @@ class CustonLineEdit:
             self._enable = True
             self._format_line()
 
+    def getObject(self):
+        return self._label_object.text()
+
     def get_text(self):
         return self._lineEdit.text()
 
     def get_state(self):
         return self._state
+
+    def set_text(self, new_value):
+        self._lineEdit.setText(new_value)
 
 
 class WinProgram(object):
@@ -161,7 +161,7 @@ class WinProgram(object):
 
         def initWindowMode(info_edit_lines):
             window = QtWidgets.QWidget(self._centralwidget)
-            window.setGeometry(0, 50, 856, 435)
+            window.setGeometry(0, 50, 856, 495)
             array_lines = []
             for i in info_edit_lines:
                 array_lines.append(CustonLineEdit())
@@ -186,14 +186,23 @@ class WinProgram(object):
                                    'QCheckBox::indicator:unchecked {image: url(imgs/checkbox/unchecked.jpg);}')
             selector.stateChanged.connect(lines[-1].change_enabled)
 
+        def initButtonFile(pos, score, img):
+            button = QtWidgets.QPushButton(self._centralwidget)
+            button.setGeometry(*pos, 40, 40)
+            button.setStyleSheet('QPushButton {image: url(%s.png);}'
+                                 'QPushButton:disabled {image: url(%s_disabled.png);}' % (img, img))
+            button.clicked.connect(lambda: self._activeFileButtons(score))
+            return button
+
         MainWindow.setFixedSize(856, 545)
         MainWindow.setWindowFlags(QtCore.Qt.CustomizeWindowHint | QtCore.Qt.WindowCloseButtonHint)
-        MainWindow.setWindowIcon(QtGui.QIcon('imgs/main_litle.png'))
+        MainWindow.setWindowIcon(QtGui.QIcon('imgs/icon/main_litle.png'))
         self._centralwidget = QtWidgets.QWidget(MainWindow)
         self._font.setPointSize(11)
         QtWidgets.QToolTip.setFont(self._font)
         self._info_label = QtWidgets.QLabel(self._centralwidget)
-        self._info_label.setPixmap(QtGui.QPixmap('imgs/info.png'))
+        self._info_label.setStyleSheet('QLabel {image: url(imgs/info/info.png)}'
+                                       'QLabel:disabled {image: url(imgs/info/info_disabled.png)}')
         self._info_label.setGeometry(801, 20, 35, 35)
         widget = QtWidgets.QWidget(self._centralwidget)
         widget.setGeometry(QtCore.QRect(274, 15, 309, 41))
@@ -219,7 +228,7 @@ class WinProgram(object):
         initCheckBox(370, self._with_reinf, self._with_reinf_lines)
         with_img_scheme = QtWidgets.QLabel(self._with_reinf)
         with_img_scheme.setGeometry(545, 20, 291, 395)
-        with_img_scheme.setPixmap(QtGui.QPixmap("imgs/with_scheme.jpg"))
+        with_img_scheme.setPixmap(QtGui.QPixmap("imgs/scheme/with_scheme.jpg"))
 
         self._without_reinf, self._without_reinf_lines = initWindowMode([[(20, 20), 'h', 16],
                                                                          [(20, 70), 'a', 16],
@@ -233,7 +242,7 @@ class WinProgram(object):
         initCheckBox(320, self._without_reinf, self._without_reinf_lines)
         without_img_scheme = QtWidgets.QLabel(self._without_reinf)
         without_img_scheme.setGeometry(545, 20, 291, 395)
-        without_img_scheme.setPixmap(QtGui.QPixmap("imgs/without_scheme.jpg"))
+        without_img_scheme.setPixmap(QtGui.QPixmap("imgs/scheme/without_scheme.jpg"))
         self._without_reinf.hide()
 
         self._start_calculations = QtWidgets.QPushButton(self._centralwidget)
@@ -253,8 +262,12 @@ class WinProgram(object):
                                              'imgs/launguage/ru_disabled.png);}')
         self._launguage_button.stateChanged.connect(self._changeLaunguage)
 
+        self._copy_button = initButtonFile((20, 485), 1, 'imgs/file/copy')
+        self._paste_button = initButtonFile((63, 485), 2, 'imgs/file/paste')
+
         self._result_menu = ResultMenu(self._with_reinf_button, self._without_reinf_button, self._info_label,
-                                       self._launguage_button, self._centralwidget)
+                                       self._launguage_button, self._copy_button, self._paste_button,
+                                       self._centralwidget)
 
         self._changeLaunguage()
         MainWindow.setCentralWidget(self._centralwidget)
@@ -306,14 +319,14 @@ class WinProgram(object):
         self._result_menu.call()
 
     @staticmethod
-    def _readFile(name):
+    def _readFileLaunguage(name):
         with open('launguage/' + name, 'rb') as file:
             info = pickle.load(file)
         return info
 
     def _initLaunguage(self):
-        self._info_ru = self._readFile('ru.lng')
-        self._info_en = self._readFile('en.lng')
+        self._info_ru = self._readFileLaunguage('ru.lng')
+        self._info_en = self._readFileLaunguage('en.lng')
 
     def _changeLaunguage(self):
         self._changed = self._info_en if self._launguage_button.isChecked() else self._info_ru
@@ -328,3 +341,31 @@ class WinProgram(object):
             called = self._changed['without_edit_unit'][i.getObject()]
             i.editUnit(called[0], called[1])
         self._result_menu.setLabelBackButton(self._changed['back'])
+
+    @staticmethod
+    def _readLines(lines):
+        array_values = []
+        for i in lines:
+            array_values.append(i.get_text())
+        return array_values
+
+    def _readFile(self):  # Функция чтения файла
+        file_link = '<ссылка на файл полученная через QFileDialog>'
+        mode = self._status_mode  # 1 - with, 2 - without
+        values = []  # Массив с новыми данными
+        # Сделай защиту, чтобы нельзя было прочитать файл сохранения with в поля without.
+        # Можно сделать первым значением в файле сохранения, переменную str: with | without.
+        # Можно сравнить _status_mode и первую переменную в файлах
+        return values
+
+    def _writeFile(self, array_value):  # Функция записи файла
+        pass
+
+    def _activeFileButtons(self, type_button):  # Функция определения кнопок по идее готова. Дальше сам. Удали коменты
+        worked = self._with_reinf_lines if self._status_mode == 1 else self._without_reinf_lines
+        if type_button == 1:
+            self._writeFile(self._readLines(worked))
+        elif type_button == 2:
+            array_new_values = self._readFile()
+            for line, value in zip(worked, array_new_values):
+                line.set_text(value)
